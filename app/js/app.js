0fdbe85c85db52f01b8e1a809ab59c663e8b1fc3
Stepan <stepan_sokolov@mail.ru>
2020-11-20 12:47:50 +0300

    Mobile menu fix загрузки

--- app/js/app.js
+++ app/js/app.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		mouseDrag: true
 	});
 
+	$('#my-menu').css("display","block")
 	$('#my-menu').mmenu({
 		"extensions": [
 			"pagedim-black"
