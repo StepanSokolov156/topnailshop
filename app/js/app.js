a7725f5334e7cf96abb17747adf1c5f66d50ec20
Stepan <stepan_sokolov@mail.ru>
2020-11-26 10:32:32 +0300

    добавил страницу Product с секцией изображения продукта (проблема с подкючением слайдера на главной)

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	var slider = document.querySelector('.my-slider');
+	// var slider = document.querySelector('.my-slider');
 
-	slider = tns({
-		container: '.my-slider',
+	// slider = tns({
+	// 	container: '.my-slider',
+	// 	items: 1,
+	// 	slideBy: 'page',
+	// 	autoplay: true,
+	// 	autoplayTimeout: 8000,
+	// 	speed: 1000,
+	// 	controls: false,
+	// 	nav: false,
+	// 	mouseDrag: true
+	// });
+
+	var productImg = document.querySelector('.product_main_img');
+
+	productImg = tns({
+		container: '.product_main_img',
+		navContainer: '.product_thumb_img',
+		loop: true,
 		items: 1,
-		slideBy: 'page',
-		autoplay: true,
-		autoplayTimeout: 8000,
-		speed: 1000,
-		controls: false,
+		navAsThumbnails: true,
+		mouseDrag: true,
+		controls: false
+	});
+
+	var productTmb = document.querySelector('.product_thumb_img');
+
+	productTmb = tns({
+		container: '.product_thumb_img',
 		nav: false,
-		mouseDrag: true
+		controls: false,
+		rewind: true,
+		autoWidth: true
 	});
 
 	$('#my-menu').css("display","block")
