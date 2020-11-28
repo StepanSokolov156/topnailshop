54d10ab8089fc6304af5d337125338f3c10b8696
StepanSokolov156 <71447612+StepanSokolov156@users.noreply.github.com>
2020-11-28 18:01:02 +0300

    fix slider (#20)

--- app/js/app.js
+++ app/js/app.js
@@ -1,42 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// var slider = document.querySelector('.my-slider');
+	var slider = document.querySelector('.my-slider'),
+		productImg = document.querySelector('.product_main_img'),
+		productTmb = document.querySelector('.product_thumb_img');
 
-	// slider = tns({
-	// 	container: '.my-slider',
-	// 	items: 1,
-	// 	slideBy: 'page',
-	// 	autoplay: true,
-	// 	autoplayTimeout: 8000,
-	// 	speed: 1000,
-	// 	controls: false,
-	// 	nav: false,
-	// 	mouseDrag: true
-	// });
 
-	var productImg = document.querySelector('.product_main_img');
+	if(slider){
+		slider = tns({
+			container: '.my-slider',
+			items: 1,
+			slideBy: 'page',
+			autoplay: true,
+			autoplayTimeout: 8000,
+			speed: 1000,
+			controls: false,
+			nav: false,
+			mouseDrag: true
+		});
+	}
+	
+	if(productImg){
+		productImg = tns({
+			container: '.product_main_img',
+			navContainer: '.product_thumb_img',
+			loop: true,
+			items: 1,
+			navAsThumbnails: true,
+			mouseDrag: true,
+			controls: false
+		});
+	}
+	
+	if(productTmb){
+		productTmb = tns({
+			container: '.product_thumb_img',
+			nav: false,
+			controls: false,
+			rewind: true,
+			autoWidth: true
+		});
+	}
 
-	productImg = tns({
-		container: '.product_main_img',
-		navContainer: '.product_thumb_img',
-		loop: true,
-		items: 1,
-		navAsThumbnails: true,
-		mouseDrag: true,
-		controls: false
-	});
+	
 
-	var productTmb = document.querySelector('.product_thumb_img');
-
-	productTmb = tns({
-		container: '.product_thumb_img',
-		nav: false,
-		controls: false,
-		rewind: true,
-		autoWidth: true
-	});
-
-	$('#my-menu').css("display","block")
 	$('#my-menu').mmenu({
 		"extensions": [
 			"pagedim-black"
